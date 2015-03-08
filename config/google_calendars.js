94ed969cca8359b7cd07c80d67d3ba41039a14da
Gordon Kwok <kwokgordon@gmail.com>
2015-03-08 00:59:20 -0700

    2015-03-08

---
+++ config/google_calendars.js
@@ -0,0 +1,39 @@
+module.exports = {
+	
+	'general': [
+		{
+			'name': 'CCAC Church',
+			'email': 'websiteccac@gmail.com',
+			'color': '%23182C57'
+		},
+	],
+	'english' : [
+		{
+			'name': 'CCAC English',
+			'email': 'otcdl1appndln1f24j0oq7qpf4@group.calendar.google.com',
+			'color': '%23125A12'
+		},
+	],
+	'cantonese' : [
+		{
+			'name': 'CCAC Cantonese',
+			'email': 'egidh3ppc18ismckttg3v7j1qs@group.calendar.google.com',
+			'color': '%23A32929'
+		},
+	],
+	'mandarin' : [
+		{
+			'name': 'CCAC Mandarin',
+			'email': 'losr4b5kb059b0tauqkb3jah14@group.calendar.google.com',
+			'color': '%23AB8B00'
+		},
+	],
+	'children': [
+		{
+			'name': 'CCAC Children',
+			'email': 'lntuk5nlro8u6nerv3q16vhv60@group.calendar.google.com',
+			'color': '%2323164E'
+		},
+	],
+};
+
---
+++ config/groups_calendars.js
@@ -0,0 +1,39 @@
+module.exports = {
+	
+	'calendars' : [
+// Life Groups - Cantonese
+		{
+			'path': '/life_groups/cantonese/bethlehem/',
+			'calendar': 'bethlehem.ccac@gmail.com'
+		},
+		{
+			'path': '/life_groups/cantonese/flames/',
+			'calendar': 'ccacflames@gmail.com'
+		},
+		{
+			'path': '/life_groups/cantonese/rainbow_2/',
+			'calendar': 'hdaskbk5orrub4rk5p20n6n4qo@group.calendar.google.com'
+		},
+// Ministries
+		{
+			'path': '/ministries/children_ministry/',
+			'calendar': 'childrens@calgarychinesealliance.org'
+		},
+		{
+			'path': '/ministries/children_awana_club/',
+			'calendar': 'childrens@calgarychinesealliance.org'
+		},
+		{
+			'path': '/ministries/children_sunday_programs/',
+			'calendar': 'childrens@calgarychinesealliance.org'
+		},
+		{
+			'path': '/ministries/children_special_events/',
+			'calendar': 'childrens@calgarychinesealliance.org'
+		},
+		{
+			'path': '/ministries/esl/',
+			'calendar': 'ccac.esl@gmail.com'
+		},
+	],
+};
