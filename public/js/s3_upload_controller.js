cc9b5ac1dfb76763babb7bc7ccdf4ea528a63aee
Gordon Kwok <kwokgordon@gmail.com>
2015-05-06 02:39:38 -0600

    2015-05-06

--- public/js/s3_upload_controller.js
+++ public/js/s3_upload_controller.js
@@ -48,7 +48,7 @@ ccac.controller('CCACController', function ($scope, $http, $log) {
  
 	$scope.upload = function(str) {
 		// Configure The S3 Object 
-		AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
+//		AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
 		AWS.config.region = $scope.creds.region;
 		var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
 
