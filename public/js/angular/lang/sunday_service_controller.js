70a65bd95f1eae3d2f8ceaca731ea5a7c87c7203
Gordon Kwok <kwokgordon@gmail.com>
2015-05-09 01:24:46 -0600

    2015-05-09

--- public/js/angular/lang/sunday_service_controller.js
+++ public/js/angular/lang/sunday_service_controller.js
@@ -1,111 +1,48 @@
 var ccac = angular.module('ccacApp', ['ui.bootstrap']);
 
-ccac.controller('SundayServiceController', function ($scope, $http, SermonListService, $log) {
+ccac.controller('SundayServiceController', function ($scope, $http, $log) {
 
 	$scope.tabs = [
-		{ title:'English', content:'english' },
-		{ title:'Cantonese', content:'cantonese' },
-		{ title:'Mandarin', content:'mandarin' }
+		{ title:'English', lang:'eng' },
+		{ title:'Cantonese', lang:'cht' },
+		{ title:'Mandarin', lang:'chs' }
 	];
 
 	$scope.formData = {};
-	
-	$scope.sermon_list = SermonListService.getSermonList();
 
-	$scope.getSermon = function() {
-		AWS.config.update({ accessKeyId: $scope.s3_access_key, secretAccessKey: 'UA5utWjUmzoy2hYH5WC+ga0mPE0V3eOjyAGxDXSL' });
-		AWS.config.region = $scope.s3_region;
-		var bucket = new AWS.S3({ params: { Bucket: $scope.s3_bucket } });
-
-		var params = { 
-		};
-	
-		bucket.listObjects(params, function(err, data) {
-			if(err) {
-				$log.info(err, err.stack);
+	$scope.init = function() {
+		var tab;
+		for(i = 0; i < $scope.tabs.length; i++) {
+			if($scope.lang == $scope.tabs[i].lang) {
+				tab = $scope.tabs[i];
+				$scope.tabs[i].active = true;
 			}
-			else {
+		}
+
+		$http.post('/api/getSermons', {congregation: tab.title})
+			.success(function(data) {
 				$log.info(data);
-				for(var i = 0; i < data.Contents.length; i++) {
-					$log.info(data.Contents[i].Key);
-				}
-				
-				var arr = sortByKeyDesc(data.Contents, 'Key');
-				$log.info(arr);
-				for(var i = 0; i < arr.length; i++) {
-					$log.info(arr[i].Key);
-				}
-				
-			}
-		})
-	};
-	
-	$scope.test = function() {
-		$scope.sermon_list = SermonListService.getSermonList();
-		$log.info($scope.sermon_list);
-
-		AWS.config.update({ accessKeyId: $scope.s3_access_key, secretAccessKey: 'UA5utWjUmzoy2hYH5WC+ga0mPE0V3eOjyAGxDXSL' });
-		AWS.config.region = $scope.s3_region;
-		var bucket = new AWS.S3({ params: { Bucket: $scope.s3_bucket } });
-
-/*
-		var params = {
-			Bucket: 'STRING_VALUE', //required
-			Delimiter: 'STRING_VALUE',
-			EncodingType: 'url',
-			Marker: 'STRING_VALUE',
-			MaxKeys: 0,
-			Prefix: 'STRING_VALUE'
-		};
-
-		s3.listObjects(params, function(err, data) {
-			if (err) console.log(err, err.stack); // an error occurred
-			else     console.log(data);           // successful response
-		});	
-*/
-		
-		var params = { 
-		};
-	
-		bucket.listObjects(params, function(err, data) {
-			if(err) {
-				$log.info(err, err.stack);
-			}
-			else {
-				$log.info(data);
-				for(var i = 0; i < data.Contents.length; i++) {
-					$log.info(data.Contents[i].Key);
-				}
-				
-				var arr = sortByKeyDesc(data.Contents, 'Key');
-				$log.info(arr);
-				for(var i = 0; i < arr.length; i++) {
-					$log.info(arr[i].Key);
-				}
-				
-			}
-		})
-	};
-})
-.service('SermonListService', function($http, $log){
-
-	this.getSermonList = function() {
-		$http.get('/api/getSermonList')
-			.success(function(data, status, headers, config) {
-				$log.info("service success");
-				$log.info(data);
-				$log.info(status);
-				$log.info(headers);
-				$log.info(config);
-				return data;
+				$scope.sermon_list = data;
 			})
-			.error(function(data, status, headers, config) {
-				$log.info("service error");
-				return { error: "Error calling /api/getSermonList" };
+			.error(function(data) {
+				$log.info("Error: " + data);
 			});
-	}
- 
-});
+	};
+	
+	$scope.sermon_list = {};
+	
+	$scope.getSermons = function(congregation) {
+		$http.post('/api/getSermons', {congregation: congregation})
+			.success(function(data) {
+				$log.info(data);
+				$scope.sermon_list = data;
+			})
+			.error(function(data) {
+				$log.info("Error: " + data);
+			});
+	};
+	
+})
 
 ////////////////////////////////////////////////////////////////////////
 // Other functions
