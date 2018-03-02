1113ceb80a90a71965b9b4a4b010ac76bdb3d768
Gordon Kwok <kwokgordon@gmail.com>
2018-03-02 14:03:20 -0700

    Updated sermon to include new fields

--- config/db.js
+++ config/db.js
@@ -1,7 +1,13 @@
-module.exports = {
-	
+var development = {
+	'db' : {
+		'mongo': 'mongodb://52.32.254.39:27017/CCAC_DEV'
+	}
+};
+
+var production = {
 	'db' : {
 		'mongo': 'mongodb://52.32.254.39:27017/CCAC'
 	}
 };
 
+module.exports = process.env.NODE_ENV === 'development' ? development : production;
